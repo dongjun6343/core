f202707264c31d2a6e0e6c045d175bbcd2272093
dongjun6343 <dongjun6343@naver.com>
2023-12-25 11:43:21 +0900

    :sparkles: 롬복과 최신 트랜드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,11 +3,13 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
@@ -15,7 +17,7 @@ public class OrderServiceImpl implements OrderService{
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); DIP 위반.
 
     //   DIP를 지킴 -> NPE발생.. -> 어떻게 해결해야할까?
-    private final  DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
     /**
      * 생성자 주입
@@ -25,12 +27,13 @@ public class OrderServiceImpl implements OrderService{
      * new OrderServiceImpl(memberRepository, discountPolicy)
      * - 자바 문법임.
      *
+     * 롬복 적용
+     * @RequiredArgsConstructor : final이 붙은 필드를 모아서 생성자를 자동으로 만들어줌.
      */
-//    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
