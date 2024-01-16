8141daf49652519e8096cdc6abc7678f8a89adba
dongjun6343 <dongjun6343@naver.com>
2024-01-16 18:36:10 +0900

    :sparkles: 스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,12 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger; // request 요청이 올때 생성됨. -> 따라서 서버를 올릴때 에러!
+    // private final MyLogger myLogger; // request 요청이 올때 생성됨. -> 따라서 서버를 올릴때 에러!
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest reqeust) {
+
         String requestURL = reqeust.getRequestURL().toString();
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+
+
         myLogger.setReqeustURL(requestURL);
 
         myLogger.log("controller test");
